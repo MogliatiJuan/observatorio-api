9b9826012744bf600b20925841b424f1352efe5e
Juan Manuel Mogliati <mogliatijm@gmail.com>
2023-09-18 23:50:29 -0300

    refactor: :art: add tables relationships

--- src/controllers/fallos/index.js
+++ src/controllers/fallos/index.js
@@ -1,14 +1,27 @@
+import { summaryVeredictDTO } from "../../dto/fallo/index.js";
 import { CreateVeredictDTO } from "../../dto/index.js";
-import { Empresas, Fallo_x_Empresa, Fallos } from "../../models/index.js";
+import Juzgados from "../../models/Juzgados/index.js";
+import {
+  Empresas,
+  Etiquetas,
+  Etiquetas_x_Fallos,
+  Fallo_x_Empresa,
+  Fallos,
+  Reclamos,
+  Rubros,
+  Tipo_Juicio,
+} from "../../models/index.js";
 
 export const veredictById = async (req, res) => {
   try {
     const { id } = req.params;
-    const file = await Fallos.findByPk(id, { include: Empresas });
+    const file = await Fallos.findByPk(id, {
+      include: [Juzgados, Tipo_Juicio, Reclamos, Rubros, Empresas, Etiquetas],
+    });
     if (!file) {
       return res.send([]);
     }
-    res.send(file);
+    res.send(new summaryVeredictDTO(file));
   } catch (error) {
     res.send(error);
   }
@@ -18,16 +31,24 @@ export const veredictsAllOrFiltered = async (req, res) => {
   try {
     const { actor, demandado, rubro } = req.query;
     const conditions = {};
+    const filesFormatted = [];
 
     actor && (conditions.agent = actor);
-    demandado && (conditions.defendant = demandado);
-    rubro && (conditions.rubro = rubro);
+
+    //reveer con nueva forma de traer los datos
+    //demandado && (conditions.defendant = demandado);
+    //rubro && (conditions.rubro = rubro);
 
     const filesFiltered = await Fallos.findAll({
       where: conditions,
+      include: [Juzgados, Tipo_Juicio, Reclamos, Rubros, Empresas, Etiquetas],
     });
 
-    res.send(filesFiltered);
+    filesFiltered.forEach((file) => {
+      filesFormatted.push(new summaryVeredictDTO(file.dataValues));
+    });
+
+    res.send(filesFormatted);
   } catch (error) {
     res.send(error);
   }
@@ -35,14 +56,14 @@ export const veredictsAllOrFiltered = async (req, res) => {
 
 export const createVeredict = async (req, res) => {
   try {
-    const { idEmpresa = [] } = req.body;
+    const { demandado = [], etiquetas = [] } = req.body;
 
     const newVeredict = new CreateVeredictDTO(req.body);
     const veredictCreated = await Fallos.create(newVeredict);
 
-    if (idEmpresa.length >= 1) {
+    if (demandado.length >= 1) {
       await Promise.all(
-        idEmpresa.map(async (id) => {
+        demandado.map(async (id) => {
           await Fallo_x_Empresa.create({
             idFallo: veredictCreated.id,
             idEmpresa: id,
@@ -51,12 +72,24 @@ export const createVeredict = async (req, res) => {
       );
     }
 
+    if (etiquetas.length >= 1) {
+      await Promise.all(
+        etiquetas.map(async (tagId) => {
+          await Etiquetas_x_Fallos.create({
+            idFallo: veredictCreated.id,
+            idTags: tagId,
+          });
+        })
+      );
+    }
+
     const falloConEmpresas = await Fallos.findByPk(veredictCreated.id, {
-      include: Empresas,
+      include: [Juzgados, Tipo_Juicio, Reclamos, Rubros, Empresas, Etiquetas],
     });
 
-    res.send(falloConEmpresas);
+    res.send(new summaryVeredictDTO(falloConEmpresas));
   } catch (error) {
+    console.log(error);
     res.send(error);
   }
 };
--- src/dto/fallo/index.js
+++ src/dto/fallo/index.js
@@ -1,16 +1,42 @@
 export class CreateVeredictDTO {
   constructor(data) {
-    this.agent = data.actor;
-    this.defendant = data.demandado;
-    this.tipojuicio = data.tipoJuicio;
-    this.causas = data.causas;
-    this.rubro = data.rubro;
-    this.fecha = data.fecha;
-    this.tribunalid = data.idTribunal;
-    this.tribunal = data.tribunal;
-    this.punitive = data.punitivo;
-    this.moral = data.moral;
-    this.patrimonial = data.patrimonial;
-    this.summary = data.resumen;
+    this.agent = data.actor || null;
+    this.tipojuicio = data.tipoJuicio || null;
+    this.causas = data.causas || null;
+    this.rubro = data.rubro || null;
+    this.fecha = data.fecha || null;
+    this.tribunalid = data.idTribunal || null;
+    this.tribunal = data.tribunal || null;
+    this.punitive = data.punitivo || null;
+    this.moral = data.moral || null;
+    this.patrimonial = data.patrimonial || null;
+    this.summary = data.resumen || null;
+  }
+}
+
+export class summaryVeredictDTO {
+  constructor(data) {
+    this.nroExpediente = data.id || null;
+    this.actor = data.agent || null;
+    this.demandado = [];
+    data.Empresas.forEach((empresa) => {
+      this.demandado.push({
+        razon_social: empresa.razon_social,
+        cuit: empresa.cuit,
+      });
+    });
+    this.tipoJuicio = data.Tipo_Juicio.description || null;
+    this.causas = data.Reclamo.description || null;
+    this.juzgado = data.Juzgado.nombre || null;
+    this.rubro = data.Rubro.rubro || null;
+    this.fecha = data.fecha || null;
+    this.punitivo = data.punitive || null;
+    this.moral = data.moral || null;
+    this.patrimonial = data.patrimonial || null;
+    this.resumen = data.summary || null;
+    this.etiquetas = [];
+    data.Etiquetas.forEach((tag) => {
+      this.etiquetas.push(tag.descriptionah);
+    });
   }
 }
