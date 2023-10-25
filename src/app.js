cd5e2cc192b95e6b23d7926a9781df90b7c0ac80
Juan Manuel <mogliatijm@gmail.com>
2023-10-25 13:36:59 -0300

    feat: :zap: add welcome message and not found

--- src/app.js
+++ src/app.js
@@ -19,6 +19,21 @@ if (config.MORGAN_LOGGING) {
   app.use(morgan(config.MORGAN_FORMAT));
 }
 app.use("/api", router);
+app.get("/", (req, res) => {
+  return res.send({
+    message: "Welcome to the Observatory API",
+    description:
+      "This API allows you to view, modify, and upload information and files related to the certains verdicts.",
+    version: "1.0.0",
+  });
+});
+app.all("*", (req, res) => {
+  res.status(404).send({
+    status: 404,
+    error: "Not Found",
+    message: "The requested resource could not be found.",
+  });
+});
 
 app.listen(config.PORT, () => {
   console.log(`Servidor activo en puerto ${config.PORT}`);
