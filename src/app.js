4aa8e1aa1a7182c26b386fe821e238a4aee82f52
Juan Manuel Mogliati <mogliatijm@gmail.com>
2024-07-04 10:56:18 -0300

    fix: :bug: upgrade limits in fileUpload

--- src/app.js
+++ src/app.js
@@ -13,7 +13,13 @@ const app = express();
 app.use("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(fileUpload({ safeFileNames: true, preserveExtension: 4 }));
+app.use(
+  fileUpload({
+    safeFileNames: true,
+    preserveExtension: 4,
+    limits: { fileSize: 50 * 1024 * 1024 },
+  })
+);
 
 if (config.MORGAN_LOGGING) {
   app.use(morgan(config.MORGAN_FORMAT));
