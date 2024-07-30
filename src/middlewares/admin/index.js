5ef196acc56109c4ede6ffc9d1ff63712f55d137
Juan Manuel Mogliati <mogliatijm@gmail.com>
2024-07-30 18:58:10 -0300

    feat: :zap: update admin middleware, use bearer token to allow or not certains endpoints

--- src/middlewares/admin/index.js
+++ src/middlewares/admin/index.js
@@ -1,12 +1,32 @@
+import jwt from "jsonwebtoken";
 import { constants } from "../../constants/index.js";
+import config from "../../config/index.js";
 
 const adminMiddleware = (req, res, next) => {
-  const { rol } = req.usuario;
+  const token = req.headers.authorization?.split(" ")[1];
 
-  if (rol !== constants.ADMIN || !rol) {
-    return res.status(403).json({ message: "Acceso no autorizado." });
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Acceso no autorizado: Token faltante" });
   }
 
-  next();
+  try {
+    const decoded = jwt.verify(token, config.SECRET_KEY);
+    const { rol: roles } = decoded;
+
+    if (!roles.some((role) => role.rol === constants.ADMIN)) {
+      return res
+        .status(403)
+        .json({ message: "Acceso prohibido: Rol no autorizado" });
+    }
+
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "Acceso no autorizado: Token inválido o expirado" });
+  }
 };
+
 export default adminMiddleware;
